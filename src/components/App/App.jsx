af3c18f03aa5217bab1f38a3cc84de65e1ff9f19
Hlieb Kotin <gleb.kotiun@gmaill.com>
2025-05-05 17:38:10 +0200

    add local storage

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -4,16 +4,17 @@ import ContactList from "../ContactList/ContactList";
 import SearchBox from "../SearchBox/SearchBox";
 import s from "./App.module.css";
 
-const initialContacts = [
-  { id: "id-1", name: "Rosie Simpson", number: "459-12-56" },
-  { id: "id-2", name: "Hermine Kline", number: "443-89-12" },
-  { id: "id-3", name: "Eden Clements", number: "645-17-79" },
-  { id: "id-4", name: "Annie Copeland", number: "227-91-26" },
-];
+const KEY_LOCALSTORAGE = "contacts";
 
 function App() {
-  const [contacts, setContacts] = useState(initialContacts);
+  const [contacts, setContacts] = useState(
+    JSON.parse(localStorage.getItem(KEY_LOCALSTORAGE)) || []
+  );
   const [filter, setFilter] = useState("");
+
+  useEffect(() => {
+    localStorage.setItem(KEY_LOCALSTORAGE, JSON.stringify(contacts));
+  }, [contacts]);
 
   const handleAdd = (newContact) => {
     console.log(newContact);
@@ -36,8 +37,6 @@ function App() {
       item.name.toLowerCase().includes(filter.toLowerCase()) ||
       item.number.toString().includes(filter.toString())
   );
-
-  console.log(filteredContacts);
 
   return (
     <div className={s.wrapper}>
